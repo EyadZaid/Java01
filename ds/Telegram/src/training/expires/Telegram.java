cede2c6dbaf7dc132604e7d2b3422a7f00937fc7
EyadZaid <eyad.zaid@hotmail.com>
2021-03-25 14:32:38 +0200

    Telegram class added

--- ds/Telegram/src/training/expires/Telegram.java
+++ ds/Telegram/src/training/expires/Telegram.java
@@ -1,4 +1,32 @@
 package training.expires;
 
+import training.expires.inputOutput.IInput;
+import training.expires.inputOutput.IOutput;
+import training.expires.manipulation.IEncoder;
+
 public class Telegram {
+
+    private IInput iInput;
+    private IOutput iOutput;
+    private IEncoder iEncoder;
+
+    public Telegram(IInput iInput, IOutput iOutput, IEncoder iEncoder){
+        this.iInput = iInput;
+        this.iOutput = iOutput;
+        this.iEncoder = iEncoder;
+    }
+
+    public void encode(){
+        String manipLine, readLine;
+
+        while (!iInput.isEnd()){
+            readLine = iInput.readline();
+            manipLine = iEncoder.encode(readLine);
+            iOutput.write(manipLine);
+        }
+        iInput.close();
+        iOutput.close();
+    }
+
+
 }
