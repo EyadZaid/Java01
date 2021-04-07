b70952617cc36f5e873d5f1e8de24459832da1bc
EyadZaid <eyad.zaid@hotmail.com>
2021-04-07 13:36:26 +0300

    Implement Main

--- ds/Generics/src/training/expires/Main.java
+++ ds/Generics/src/training/expires/Main.java
@@ -1,5 +1,6 @@
 package training.expires;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,8 +8,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        var list =  Arrays.asList(4.5, 20, 15, 5.511);
-        System.out.println(Utils.averageOfList(list));
+        List<Double> list = new ArrayList<>();
+        list.add(10.0);
+        list.add(5.2);
+        list.add(4.3);
+        list.add(20.5);
 
+        System.out.println(Utils.averageOfList(list));
+        System.out.println(Utils.midElementOfList(list));
+        System.out.println(Utils.minAndMaxElementInList(list));
+        System.out.println(Utils.bubbleSortList(list));
+        System.out.println(Utils.removeMinInList(list));
     }
 }
