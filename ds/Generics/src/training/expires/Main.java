9aee1c7f6be4ee0f2dfe41c5bdbd3e743e562971
EyadZaid <eyad.zaid@hotmail.com>
2021-04-07 13:23:54 +0300

    Implement Main

--- ds/Generics/src/training/expires/Main.java
+++ ds/Generics/src/training/expires/Main.java
@@ -1,8 +1,14 @@
 package training.expires;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        var list =  Arrays.asList(4.5, 20, 15, 5.511);
+        System.out.println(Utils.averageOfList(list));
+
     }
 }
--- ds/Generics/src/training/expires/Utils.java
+++ ds/Generics/src/training/expires/Utils.java
@@ -109,14 +109,12 @@ public class Utils {
         for (int i=0; i<size-1; i++) {
             for (int j=0; j< size-i-1; j++) {
                 if (list.get(j).compareTo(list.get(j+1)) > 0) {
-
                     T temp = list.get(j);
                     list.set(j, list.get(j+1));
                     list.set(j+1, temp);
                 }
             }
         }
-
         return list;
     }
 
