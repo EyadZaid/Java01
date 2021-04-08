ede6fc8ce5fc2d0b269a6a04ea007d685289d741
EyadZaid <eyad.zaid@hotmail.com>
2021-04-08 08:16:12 +0300

    RemoveMinTest tests added

--- ds/Generics/src/training/expires/tests/RemoveMinTest.java
+++ ds/Generics/src/training/expires/tests/RemoveMinTest.java
@@ -30,4 +30,38 @@ class RemoveMinTest {
         list.set(2, v2);
         assertEquals(v1, Utils.removeMinInList(list));
     }
+
+
+    @Test
+    void removeMinInList_EmptyList() {
+        List<Double> list = new ArrayList<>();
+        assertEquals(null, Utils.removeMinInList(list));
+    }
+
+
+    @Test
+    void removeMinInList_Duplicated() {
+        List<Double> list = new ArrayList<>();
+        list.add(10.0);
+        list.add(10.0);
+        list.add(10.0);
+        list.add(10.5);
+        double n = 10;
+        var v1= Double.valueOf(n);
+        var v2= Double.valueOf(n);
+        list.set(0,v1);
+        list.set(2, v2);
+        assertEquals(v1, Utils.removeMinInList(list));
+    }
+
+
+    @Test
+    void removeMinInList_OneElement() {
+        List<Double> list = new ArrayList<>();
+        list.add(10.0);
+        double n = 10;
+        var v1= Double.valueOf(n);
+        list.set(0,v1);
+        assertEquals(v1, Utils.removeMinInList(list));
+    }
 }
