797cf12f2c59ea933ff163a50407a73242542ab2
EyadZaid <eyad.zaid@hotmail.com>
2021-04-11 13:44:29 +0300

    List / DoublyLinkedList: Added list iterator test

---
+++ ds/list/src/training/expires/tests/ListIteratorTest.java
@@ -0,0 +1,15 @@
+package training.expires.tests;
+
+import org.junit.jupiter.api.Test;
+
+class ListIteratorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listIteratorTest(){
+
+    }
+}
