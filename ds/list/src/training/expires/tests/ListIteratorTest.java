df49ca06d9cdb5f6a5110bc5e8d0dcbc68d18f7b
EyadZaid <eyad.zaid@hotmail.com>
2021-04-11 14:02:35 +0300

    List / DoublyLinkedList: Implement list iterator test

--- ds/list/src/training/expires/tests/ListIteratorTest.java
+++ ds/list/src/training/expires/tests/ListIteratorTest.java
@@ -1,15 +1,45 @@
 package training.expires.tests;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+import training.expires.DoublyLinkedList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 class ListIteratorTest {
+    private DoublyLinkedList<Integer> list;
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
+        list = new DoublyLinkedList<>();
     }
 
     @Test
     void listIteratorTest(){
+        var givenList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> resultList = new ArrayList<>();
 
+        for (int i=0; i<givenList.size(); i++){
+
+        }
+
+        for (var item : givenList){
+            list.addAtTail(item);
+        }
+
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()){
+            resultList.add(iterator.next());
+        }
+
+        assertEquals(givenList.size(), resultList.size());
+
+        for (int i=0; i<givenList.size(); i++){
+            assertEquals(givenList.get(i), resultList.get(i));
+        }
     }
 }
