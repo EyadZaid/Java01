c975a63a2e923e07c1fbbe8377671aa6230c37a4
EyadZaid <eyad.zaid@hotmail.com>
2021-04-12 15:30:08 +0300

    Exam01 : Added new tests to NonRepeating

--- ds/Exam01/src/training/expires/tests/NonRepeatingGenericTest.java
+++ ds/Exam01/src/training/expires/tests/NonRepeatingGenericTest.java
@@ -14,8 +14,20 @@ class NonRepeatingGenericTest {
     }
 
     @Test
-    void first_non_repeating() {
+    void first_non_repeatingTest() {
         String array[] = { "1", "5" ,"3", "4", "2", "9", "3", "1", "7", "5", "1","2"};
         assertEquals("4", NonRepeatingGeneric.first_non_repeating(array));
+    }
+
+    @Test
+    void first_non_repeatingEmptyArrayTest() {
+        String array[] = {};
+        assertEquals(null, NonRepeatingGeneric.first_non_repeating(array));
+    }
+
+    @Test
+    void first_non_repeatingOneItemInArrayTest() {
+        String array[] = {"10"};
+        assertEquals("10", NonRepeatingGeneric.first_non_repeating(array));
     }
 }
--- ds/Exam01/src/training/expires/tests/NonRepeatingNumTest.java
+++ ds/Exam01/src/training/expires/tests/NonRepeatingNumTest.java
@@ -1,5 +1,7 @@
 package training.expires.tests;
 
+import org.junit.jupiter.api.Test;
+import training.expires.NonRepeatingGeneric;
 import training.expires.NonRepeatingNum;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,5 +16,17 @@ class NonRepeatingNumTest {
     void first_non_repeating_num() {
         int array[] = {1,5,3,4,2,9,3,1,7,5,1,2};
         assertEquals(4, NonRepeatingNum.first_non_repeating_num(array));
+    }
+
+    @Test
+    void first_non_repeatingEmptyArrayTest() {
+        int[] array = {};
+        assertEquals(null, NonRepeatingNum.first_non_repeating_num(array));
+    }
+
+    @Test
+    void first_non_repeatingOneItemInArrayTest() {
+        int[] array = {10};
+        assertEquals(10, NonRepeatingNum.first_non_repeating_num(array));
     }
 }
