5947ff7e8d3ba2b913783dd4054a58d40f590c6b
EyadZaid <eyad.zaid@hotmail.com>
2021-04-12 13:16:30 +0300

    Exam01 : Implement first_non_repeating_num function

--- ds/Exam01/src/training/expires/NonRepeatingNum.java
+++ ds/Exam01/src/training/expires/NonRepeatingNum.java
@@ -1,8 +1,27 @@
 package training.expires;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NonRepeatingNum {
 
-    public static int first_non_repeating_num(int array[]){
-        return 0;
+    public static Integer first_non_repeating_num(int array[]){
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        for (var e : array){
+            if (hashMap.containsKey(e)) {
+                hashMap.put(e, hashMap.get(e) + 1);
+            }
+            else {
+                hashMap.put(e, 1);
+            }
+        }
+
+        for (var v : hashMap.entrySet()) {
+            if (v.getValue() == 1) {
+                return v.getKey();
+            }
+        }
+
+        return null;
     }
 }
