dd475ab5cb9f44dccf59d713af125632a11b3b05
EyadZaid <eyad.zaid@hotmail.com>
2021-04-17 00:19:00 +0300

    Threads: Added test negative to CountPairsSum

--- ds/Threads/src/training/expires/tests/CountPairsSumTest.java
+++ ds/Threads/src/training/expires/tests/CountPairsSumTest.java
@@ -35,6 +35,16 @@ class CountPairsSumTest {
     }
 
     @Test
+    void countPairsWithSumThreeWithNegative() {
+        int[] arr = {1, 3, 2, 2, 2, 4, 7, 5, 19, -4, 46, -2, -56};
+
+        CountPairsSum countPairsSum = new CountPairsSum();
+
+        int count = countPairsSum.countPairsWithSum(arr, 3, 5);
+        assertEquals(3, count);
+    }
+
+    @Test
     void countPairsWithSumEmptyArray() {
         int[] arr = {};
 
