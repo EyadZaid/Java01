e2b7a7a35653fdd59d41fd52abce29f43612ea63
EyadZaid <eyad.zaid@hotmail.com>
2021-04-16 22:51:16 +0300

    Threads: Added test to ArraySort

--- ds/Threads/src/training/expires/tests/ArraySortTest.java
+++ ds/Threads/src/training/expires/tests/ArraySortTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import training.expires.ArraySort;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArraySortTest {
@@ -13,7 +16,7 @@ class ArraySortTest {
     }
 
     @Test
-    void arraySortTest() {
+    void arraySortTestSmallArray() {
         int[] arr = {3, 6, 8, 2, 4, 1, 0, 7, 5, 6};
         int[] arrExpected = {0, 1, 2, 3, 4, 5, 6, 6, 7, 8};
 
@@ -21,7 +24,26 @@ class ArraySortTest {
         int[] sortedArr = arraySort.sort();
 
         for (int i=0; i<arr.length; i++){
-            System.out.println(sortedArr[i]);
+            assertEquals(arrExpected[i], sortedArr[i]);
+        }
+    }
+
+    @Test
+    void arraySortTestRandomArray() {
+        Random rng = new Random();
+        int[] arr = new int[100];
+        int[] arrExpected = new int[100];
+        for (int i=0; i<arr.length; i++){
+            int rnd = rng.nextInt(1000 - 10)  + 10;
+            arr[i] = rnd;
+            arrExpected[i] = rnd;
+        }
+
+        ArraySort arraySort = new ArraySort(arr);
+        int[] sortedArr = arraySort.sort();
+        Arrays.sort(arrExpected);
+
+        for (int i=0; i<arr.length; i++){
             assertEquals(arrExpected[i], sortedArr[i]);
         }
     }
