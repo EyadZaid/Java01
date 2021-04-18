a3c8bdce9dce1285f124b8364e1421989f773e69
EyadZaid <eyad.zaid@hotmail.com>
2021-04-18 13:55:06 +0300

    ThreadSafeQueue: Implement enqueue

--- ds/Thread_safe_queue/src/training/expires/ThreadSafeQueue.java
+++ ds/Thread_safe_queue/src/training/expires/ThreadSafeQueue.java
@@ -4,11 +4,15 @@ public class ThreadSafeQueue<T> {
     private T[] data;
     private int capacity;
     private int size;
+    private int front;
+    private int rear;
 
     public ThreadSafeQueue(int capacity) {
         this.capacity = capacity;
         this.data = (T[]) new Object[capacity];
         this.size = 0;
+        this.front = 0;
+        this.rear = capacity - 1;
     }
 
     public int getSize(){
@@ -23,6 +27,15 @@ public class ThreadSafeQueue<T> {
         return size == capacity;
     }
 
+    public void enqueue(T item){
+        if (isFull()){
+            return;
+        }
+
+        rear = (rear + 1)  % capacity;
+        data[rear] = item;
+        size++;
+    }
 
 
 
