c6ccea8e0f95a166c50126507f2d9db13cbeaca2
EyadZaid <eyad.zaid@hotmail.com>
2021-04-18 22:20:30 +0300

    ThreadSafeQueue: Update Producer

--- ds/Thread_safe_queue/src/training/expires/Producer.java
+++ ds/Thread_safe_queue/src/training/expires/Producer.java
@@ -1,19 +1,21 @@
 package training.expires;
 
 public class Producer implements Runnable{
-    private ThreadSafeQueue<Integer> queue;
+    private final ThreadSafeQueue<Integer> queue;
     private final int number;
 
     public Producer(ThreadSafeQueue<Integer> queue, int number) {
+        if (queue == null || number < 1){
+            throw new IllegalStateException();
+        }
         this.queue = queue;
         this.number = number;
     }
 
     @Override
     public void run() {
-        for (int i=0; i<15; i++){
+        for (int i=0; i<number; i++){
             queue.enqueue(i);
-            System.out.println("Producer " + number + " enqueue: " + i);
         }
     }
 }
