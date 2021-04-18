0605c98f6a389fdc464eae0f2053e080da5900e1
EyadZaid <eyad.zaid@hotmail.com>
2021-04-18 13:36:36 +0300

    ThreadSafeQueue: Initialize new class ThreadSafeQueue

--- ds/Thread_safe_queue/src/training/expires/ThreadSafeQueue.java
+++ ds/Thread_safe_queue/src/training/expires/ThreadSafeQueue.java
@@ -1,4 +1,14 @@
 package training.expires;
 
-public class ThreadSafeQueue {
+public class ThreadSafeQueue<T> {
+    private T[] data;
+    private int capacity;
+    private int size;
+
+    public ThreadSafeQueue(int capacity) {
+        this.capacity = capacity;
+        this.data = (T[]) new Object[capacity];
+    }
+
+
 }
