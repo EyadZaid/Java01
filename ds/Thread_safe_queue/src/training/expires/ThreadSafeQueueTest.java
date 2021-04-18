2599530640c673abc37e618be9ee1138578bddc6
EyadZaid <eyad.zaid@hotmail.com>
2021-04-18 15:11:29 +0300

    ThreadSafeQueue: Added test to producer and consumer

--- ds/Thread_safe_queue/src/training/expires/ThreadSafeQueueTest.java
+++ ds/Thread_safe_queue/src/training/expires/ThreadSafeQueueTest.java
@@ -5,15 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ThreadSafeQueueTest {
+    private ThreadSafeQueue<Integer> queue;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        queue = new ThreadSafeQueue<>(10);
     }
 
     @Test
-    void FIFOTest() {
-        ThreadSafeQueue<Integer> queue = new ThreadSafeQueue<>(10);
-
+    void FIFOTest_oneThread() {
         for (int i=0; i<10; i++){
             queue.enqueue(i);
         }
@@ -21,5 +21,17 @@ class ThreadSafeQueueTest {
         for (int i=0; i<10; i++){
             assertEquals(i, queue.dequeue());
         }
+    }
+
+    @Test
+    void twoThreadTest() {
+        Producer producer1 = new Producer(queue, 1);
+        Consumer consumer1 = new Consumer(queue, 1);
+
+        Thread tProducer1 = new Thread(producer1);
+        Thread tConsumer1 = new Thread(consumer1);
+
+        tProducer1.start();
+        tConsumer1.start();
     }
 }
