fa38fe50069c02e2104870efe9464dbd6ba65693
EyadZaid <eyad.zaid@hotmail.com>
2021-04-18 22:19:08 +0300

    ThreadSafeQueue: Update Consumer

--- ds/Thread_safe_queue/src/training/expires/Consumer.java
+++ ds/Thread_safe_queue/src/training/expires/Consumer.java
@@ -1,19 +1,30 @@
 package training.expires;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Consumer implements Runnable{
-    private ThreadSafeQueue<Integer> queue;
+    private final ThreadSafeQueue<Integer> queue;
+    private final List<Integer> result;
     private final int number;
 
     public Consumer(ThreadSafeQueue<Integer> queue, int number) {
+        if (queue == null || number < 1){
+            throw new IllegalStateException();
+        }
         this.queue = queue;
+        this.result = new ArrayList<>();
         this.number = number;
     }
 
     @Override
     public void run() {
-        for (int i=0; i<10; i++){
-            System.out.println("Consumer " + number + " dequeue: " + queue.dequeue());
+        for (int i=0; i<number; i++){
+            result.add(queue.dequeue());
         }
+    }
 
+    public List<Integer> getResult() {
+        return result;
     }
 }
