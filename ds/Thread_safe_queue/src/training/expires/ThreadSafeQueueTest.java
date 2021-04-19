796211f1bc1ed44676c33dbdfb02bfd8068057ab
EyadZaid <eyad.zaid@hotmail.com>
2021-04-19 12:41:02 +0300

    ThreadSafeQueue: Added test with 1 producers 2 consumer

--- ds/Thread_safe_queue/src/training/expires/ThreadSafeQueueTest.java
+++ ds/Thread_safe_queue/src/training/expires/ThreadSafeQueueTest.java
@@ -84,6 +84,42 @@ class ThreadSafeQueueTest {
         assertTrue(checkResultFor_2producers(list, consumer.getResult()));
     }
 
+    @Test
+    void testWith_1producer_2consumer() {
+        int capacity = 1000;
+        int enqueue_n = 10000;  //for 1 producer
+        int dequeue_n = enqueue_n / 2; //for 1 consumer
+
+        queue = new ThreadSafeQueue<>(capacity);
+        List<Integer> list = generateList(enqueue_n);
+
+        Producer producer = new Producer(queue, list);
+        Consumer consumer1 = new Consumer(queue, dequeue_n);
+        Consumer consumer2 = new Consumer(queue, dequeue_n);
+
+        Thread tProducer = new Thread(producer);
+        Thread tConsumer1 = new Thread(consumer1);
+        Thread tConsumer2 = new Thread(consumer2);
+
+        tProducer.start();
+        tConsumer1.start();
+        tConsumer2.start();
+
+        try {
+            tProducer.join();
+            tConsumer1.join();
+            tConsumer2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            fail();
+        }
+
+        assertTrue(queue.isEmpty());
+        assertEquals(dequeue_n, consumer1.getResult().size());
+        assertEquals(dequeue_n, consumer2.getResult().size());
+        assertTrue(checkResultFor_2consumers(list, consumer1.getResult(), consumer2.getResult()));
+    }
+
 
     private boolean checkResultFor_2producers(List<Integer> list, List<Integer> result) {
         int list_size = list.size();
@@ -91,17 +127,38 @@ class ThreadSafeQueueTest {
             return false;
         }
 
-        List<Integer> list1 = new ArrayList<>(list);
-        List<Integer> list2 = new ArrayList<>(list);
-
         int i = 0, j = 0;
         for (int k=0; k<result.size(); k++){
-            if (result.get(k) == list1.get(i)){
+            if (result.get(k) == list.get(i)){
                 i = (i < list_size-1) ? (i + 1) : i;
             }
             else {
-                if (result.get(k) == list2.get(j)){
+                if (result.get(k) == list.get(j)){
                     j = (j < list_size-1) ? (j + 1) : j;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkResultFor_2consumers(List<Integer> list, List<Integer> result1, List<Integer> result2) {
+        if (list.size() != (result1.size() + result2.size())){
+            return false;
+        }
+        int result_size = result1.size();
+
+        int i = 0, j = 0;
+        for (int k=0; k<list.size(); k++){
+            if (list.get(k) == result1.get(i)){
+                i = (i < result_size-1) ? (i + 1) : i;
+            }
+            else {
+                if (list.get(k) == result2.get(j)){
+                    j = (j < result_size-1) ? (j + 1) : j;
                 }
                 else {
                     return false;
