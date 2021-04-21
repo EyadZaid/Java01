1ec867aeb0f2ada81b24d47155d56735aaad5e63
EyadZaid <eyad.zaid@hotmail.com>
2021-04-21 13:40:58 +0300

    PeriodicScheduler: Added test to stopAll()

--- ds/PeriodicScheduler/src/training/expires/PeriodicSchedulerTest.java
+++ ds/PeriodicScheduler/src/training/expires/PeriodicSchedulerTest.java
@@ -14,8 +14,28 @@ class PeriodicSchedulerTest {
         scheduler = new PeriodicScheduler();
     }
 
+
     @Test
-    void scheduleTest() {
+    void schedulerStopAllTest() {
+        Task task1 = new Task(() -> System.out.println("Task #1 running"));
+        Task task2 = new Task(() -> System.out.println("Task #2 running"));
+
+        scheduler.schedule(task1, 1000, TimeUnit.MILLISECONDS);
+        scheduler.schedule(task2, 1000, TimeUnit.MILLISECONDS);
+
+        try {
+            Thread.sleep(3000);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        scheduler.stopAll();
+    }
+
+
+    @Test
+    void schedulerTest() {
         Task task1 = new Task(() -> System.out.println("Task #1 running"));
         Task task2 = new Task(() -> System.out.println("Task #2 running"));
 
