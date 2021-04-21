8e44807194c69b6029525c24c0fb95f0ef185d4a
EyadZaid <eyad.zaid@hotmail.com>
2021-04-21 23:34:12 +0300

    PeriodicScheduler: Move function from TaskRunnable class to Task class

--- ds/PeriodicScheduler/src/training/expires/Task.java
+++ ds/PeriodicScheduler/src/training/expires/Task.java
@@ -1,24 +1,92 @@
 package training.expires;
 
-public class Task {
-    private Action func;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Task implements Runnable{
+    private final Runnable runnable;
+    private final Lock guard;
+    private final Condition running;
     private TaskStatus status;
+    private long period;
+    private TimeUnit unit;
 
 
-    public Task(Action func) {
-        this.func = func;
-        status = TaskStatus.STOPPED;
+    public Task(Runnable runnable, long period, TimeUnit unit) {
+        this.runnable = runnable;
+        this.period = period;
+        this.unit = unit;
+        status = TaskStatus.RUNNING;
+        guard = new ReentrantLock();
+        running = guard.newCondition();
     }
 
-    public void execute() {
-        func.apply();
+    private void executeTask() {
+        long start = System.nanoTime();
+
+        runnable.run();
+
+        long timeSpan = System.nanoTime() - start;
+        long waitTimeNano =  unit.toNanos(period) - timeSpan;
+
+        if (waitTimeNano < 0) {
+            waitTimeNano = 0;
+        }
+
+        try {
+            running.awaitNanos(waitTimeNano);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void setStatus(TaskStatus status) {
-        this.status = status;
+    public void updateStatus(TaskStatus status) {
+        guard.lock();
+        try {
+            this.status = status;
+        }
+        finally {
+            guard.unlock();
+        }
     }
 
-    public TaskStatus getStatus() {
-        return status;
+    public void reschedule(long period, TimeUnit unit) {
+        guard.lock();
+        try {
+            this.period = period;
+            this.unit = unit;
+        }
+        finally {
+            guard.unlock();
+        }
     }
+
+    @Override
+    public void run() {
+        guard.lock();
+        while (status != TaskStatus.STOPPED) {
+            try {
+                while (status == TaskStatus.SUSPENDED) {
+                    running.await();
+                }
+                guard.unlock();
+                executeTask();
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            finally {
+                guard.unlock();
+            }
+        }
+        guard.unlock();
+    }
+
+
+
+
+
+
 }
