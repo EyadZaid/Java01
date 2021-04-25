f2b91b4926de5557ccdc6dbc9c561466f64f4482
EyadZaid <eyad.zaid@hotmail.com>
2021-04-25 14:32:47 +0300

    QuickSort: QuickSort thread pool Tested

---
+++ ds/QuicksortThreadPool/src/training/expires/QuickSortThreadsTest.java
@@ -0,0 +1,30 @@
+package training.expires;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuickSortThreadsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void quickSortThreadTest() {
+        Integer[] arr = {4, 9, 1, -2, 46, 98, 34, -16, 7, 6};
+        Integer[] expectedArr = Arrays.copyOfRange(arr, 0, arr.length);
+
+        QuickSortThreads<Integer> quickSort = new QuickSortThreads<>(0, arr.length-1, arr);
+
+        quickSort.sort();
+
+        Arrays.sort(expectedArr);
+
+        //System.out.println(Arrays.toString(arr));
+        assertArrayEquals(expectedArr, arr);
+    }
+}
