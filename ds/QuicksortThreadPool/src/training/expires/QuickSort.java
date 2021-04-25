a977570190efbc8ea64d287053c5668fe5f23516
EyadZaid <eyad.zaid@hotmail.com>
2021-04-25 13:42:37 +0300

    QuickSort: added test for QuickSort

--- ds/QuicksortThreadPool/src/training/expires/QuickSort.java
+++ ds/QuicksortThreadPool/src/training/expires/QuickSort.java
@@ -1,36 +1,50 @@
 package training.expires;
 
+import java.util.Arrays;
+
 public class QuickSort<T extends Comparable<T>> {
+    private final T[] arr;
 
-    public void quickSort(T[] arr, int low, int high)
-    {
+    public QuickSort(T[] arr) {
+        this.arr = arr;
+    }
+
+    public void sort() {
+        quickSort(0, arr.length-1);
+    }
+
+    private void quickSort(int low, int high) {
         if (low < high) {
-            int pi = partition(arr, low, high);
+            int pivot = partition(low, high);
 
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+            quickSort(low, pivot - 1);
+            quickSort(pivot + 1, high);
         }
     }
 
-    public int partition(T[] arr, int low, int high) {
+    private int partition(int low, int high) {
         T pivot = arr[high];
         int i = (low - 1);
 
         for(int j = low; j <= high - 1; j++) {
             if (arr[j].compareTo(pivot) < 0) {
                 i++;
-                swap(arr, i, j);
+                swap(i, j);
             }
         }
-        swap(arr, i + 1, high);
+        swap(i + 1, high);
         return (i + 1);
     }
 
 
-    public void swap(T[] arr, int i, int j) {
+    private void swap(int i, int j) {
         T temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
+    @Override
+    public String toString() {
+        return Arrays.toString(arr);
+    }
 }
