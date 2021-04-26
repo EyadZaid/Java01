6c71b5d49a2104c54fcaa1c7545936a87215558d
EyadZaid <eyad.zaid@hotmail.com>
2021-04-26 12:50:25 +0300

    Web Movies: Start with HttpClient

--- proj/movies/WebMovies/src/training/expires/Main.java
+++ proj/movies/WebMovies/src/training/expires/Main.java
@@ -1,8 +1,27 @@
 package training.expires;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://www.omdbapi.com/?s=soul&apikey=a4038bc6"))
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(response.body());
     }
 }
