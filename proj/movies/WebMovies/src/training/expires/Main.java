dbb4fdc82cb016a813e0ed45792d41cf40bb2069
EyadZaid <eyad.zaid@hotmail.com>
2021-04-26 21:53:44 +0300

    Web Movies: Implement function get movies by ids

--- proj/movies/WebMovies/src/training/expires/Main.java
+++ proj/movies/WebMovies/src/training/expires/Main.java
@@ -9,8 +9,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
 
@@ -46,20 +46,49 @@ public class Main {
             for (int i = 0; i < arr.length(); i++) {
                 String imdb_id = arr.getJSONObject(i).getString("imdbID");
                 imdbIDsList.add(imdb_id);
-                System.out.println(imdb_id);
-            }
-
+              }
         }
 
 
-
-
-
-
-
-
+        getMovies(imdbIDsList);
 
 
         //System.out.println(response.body());
+    }
+
+    public static void getMovies(List<String> imdbIDsList) {
+        List<Movie> moviesList = new ArrayList<>();
+
+        for (var imdbID : imdbIDsList) {
+            String apiKey = "a4038bc6";
+            String urlSearchByID = "http://www.omdbapi.com/?i=" + imdbID + "&apikey=" + apiKey;
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urlSearchByID)).build();
+
+            HttpResponse<String> response = null;
+            try {
+                response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (response != null) {
+                JSONObject obj = new JSONObject(response.body());
+
+                String title = obj.getString("Title");
+                String rated = obj.getString("Rated");
+                String runtime = obj.getString("Runtime");
+                String year = obj.getString("Year");
+
+                String directors = obj.getString("Director");
+                List<String> directorsList = Arrays.asList(directors.split("\\s*,\\s*"));
+
+                String genres = obj.getString("Genre");
+                List<String> genreList = Arrays.asList(genres.split("\\s*,\\s*"));
+
+                Movie movie = new Movie(imdbID, title, rated, runtime, year, directorsList, genreList);
+                moviesList.add(movie);
+            }
+        }
     }
 }
