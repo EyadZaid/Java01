edf2f7cfcd276b817c96058d490574e9f2efc816
EyadZaid <eyad.zaid@hotmail.com>
2021-04-26 18:23:33 +0300

    Web Movies: Added JsonObject and used it

--- proj/movies/WebMovies/src/training/expires/Main.java
+++ proj/movies/WebMovies/src/training/expires/Main.java
@@ -1,19 +1,34 @@
 package training.expires;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
+    /// http://www.omdbapi.com/?s=soul&apikey=a4038bc6
+    /// http://www.omdbapi.com/?i=tt2948372&apikey=a4038bc6
+
+
     public static void main(String[] args) {
 
+        String apiKey = "a4038bc6";
+        String title = "soul";
+        String urlSearchByTitle = "http://www.omdbapi.com/?s=" + title + "&apikey=" + apiKey;
+
+        String imdbID = "tt2948372";
+        String urlSearchByID = "http://www.omdbapi.com/?i=" + imdbID + "&apikey=" + apiKey;
+
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?s=soul&apikey=a4038bc6"))
-                .build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urlSearchByTitle)).build();
 
         HttpResponse<String> response = null;
         try {
@@ -23,7 +38,28 @@ public class Main {
         }
 
 
+        List<String> imdbIDsList = new ArrayList<>();
 
-        System.out.println(response.body());
+        if (request != null) {
+            JSONObject obj = new JSONObject(response.body());
+            JSONArray arr = obj.getJSONArray("Search");
+            for (int i = 0; i < arr.length(); i++) {
+                String imdb_id = arr.getJSONObject(i).getString("imdbID");
+                imdbIDsList.add(imdb_id);
+                System.out.println(imdb_id);
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+        //System.out.println(response.body());
     }
 }
