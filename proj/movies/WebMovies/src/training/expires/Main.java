825e6b5dec8636b717ad33457b4540b0d20ea495
EyadZaid <eyad.zaid@hotmail.com>
2021-04-26 23:54:34 +0300

    Web Movies: Implement getMoviesByIDs function

--- proj/movies/WebMovies/src/training/expires/Main.java
+++ proj/movies/WebMovies/src/training/expires/Main.java
@@ -1,95 +1,9 @@
 package training.expires;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import training.expires.data.Movie;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
-
-    /// http://www.omdbapi.com/?s=soul&apikey=a4038bc6
-    /// http://www.omdbapi.com/?i=tt2948372&apikey=a4038bc6
-
 
     public static void main(String[] args) {
 
-        String apiKey = "a4038bc6";
-        String title = "soul";
-        String urlSearchByTitle = "http://www.omdbapi.com/?s=" + title + "&apikey=" + apiKey;
-
-        String imdbID = "tt2948372";
-        String urlSearchByID = "http://www.omdbapi.com/?i=" + imdbID + "&apikey=" + apiKey;
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urlSearchByTitle)).build();
-
-        HttpResponse<String> response = null;
-        try {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
-        List<String> imdbIDsList = new ArrayList<>();
-
-        if (request != null) {
-            JSONObject obj = new JSONObject(response.body());
-            JSONArray arr = obj.getJSONArray("Search");
-            for (int i = 0; i < arr.length(); i++) {
-                String imdb_id = arr.getJSONObject(i).getString("imdbID");
-                imdbIDsList.add(imdb_id);
-              }
-        }
-
-
-        getMovies(imdbIDsList);
-
-
-        //System.out.println(response.body());
     }
 
-    public static void getMovies(List<String> imdbIDsList) {
-        List<Movie> moviesList = new ArrayList<>();
-
-        for (var imdbID : imdbIDsList) {
-            String apiKey = "a4038bc6";
-            String urlSearchByID = "http://www.omdbapi.com/?i=" + imdbID + "&apikey=" + apiKey;
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(urlSearchByID)).build();
-
-            HttpResponse<String> response = null;
-            try {
-                response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            if (response != null) {
-                JSONObject obj = new JSONObject(response.body());
-
-                String title = obj.getString("Title");
-                String rated = obj.getString("Rated");
-                String runtime = obj.getString("Runtime");
-                String year = obj.getString("Year");
-
-                String directors = obj.getString("Director");
-                List<String> directorsList = Arrays.asList(directors.split("\\s*,\\s*"));
-
-                String genres = obj.getString("Genre");
-                List<String> genreList = Arrays.asList(genres.split("\\s*,\\s*"));
-
-                Movie movie = new Movie(imdbID, title, rated, runtime, year, directorsList, genreList);
-                moviesList.add(movie);
-            }
-        }
-    }
 }
--- proj/movies/WebMovies/src/training/expires/logic/queries/SearchByTitle.java
+++ proj/movies/WebMovies/src/training/expires/logic/queries/SearchByTitle.java
@@ -2,6 +2,7 @@ package training.expires.logic.queries;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import training.expires.data.Movie;
 import training.expires.logic.RequestHttp;
 
 import java.util.ArrayList;
@@ -15,7 +16,7 @@ public class SearchByTitle {
         requestHttp = new RequestHttp();
     }
 
-    public List<String> search(String title) {
+    public List<Movie> search(String title) {
         String url = "http://www.omdbapi.com/?s=" + title + "&apikey=" + API_KEY;
         String jsonString = requestHttp.getRequest(url);
         List<String> imdbIDsList = new ArrayList<>();
@@ -27,7 +28,17 @@ public class SearchByTitle {
             imdbIDsList.add(imdb_id);
         }
 
-        return imdbIDsList;
+        return getMoviesByIDs(imdbIDsList);
+    }
+
+
+    private List<Movie> getMoviesByIDs(List<String> imdbIDsList) {
+        SearchByImdbId searchById = new SearchByImdbId();
+        List<Movie> movies = new ArrayList<>();
+        for (var id : imdbIDsList) {
+            movies.add(searchById.search(id));
+        }
+        return movies;
     }
 
 
