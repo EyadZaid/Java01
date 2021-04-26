c6eaad2cb1da2bdfe881474d1c47834b8bc98bd8
EyadZaid <eyad.zaid@hotmail.com>
2021-04-26 22:53:11 +0300

    Web Movies: Implement RequestHttp class

--- proj/movies/WebMovies/src/training/expires/logic/RequestHttp.java
+++ proj/movies/WebMovies/src/training/expires/logic/RequestHttp.java
@@ -1,5 +1,31 @@
 package training.expires.logic;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class RequestHttp {
+
+    public String getRequest(String url) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (response == null ) {
+            throw new IllegalStateException();
+        }
+
+        return response.body();
+    }
+
+
 
 }
