2724efcd34e04c83697b1659b39851f54b5e779f
EyadZaid <eyad.zaid@hotmail.com>
2021-04-27 19:05:17 +0300

    Web Movies: tests updated

--- proj/movies/WebMovies/src/training/expires/tests/QueriesTest.java
+++ proj/movies/WebMovies/src/training/expires/tests/QueriesTest.java
@@ -17,59 +17,57 @@ class QueriesTest {
         query = UserQuery.getInstance();
     }
 
-    /*
+
     @Test
     void searchByImdbIdTest() {
         String idToSearch1 = "tt2948372";
-        Movie movie1 = query.search(QueryType.IMDB_ID, idToSearch1).get(0);
+        Movie movie1 = query.searchById(idToSearch1);
         assertEquals(idToSearch1, movie1.getImdbID());
 
         String idToSearch2 = "tt0220264";
-        Movie movie2 = query.search(QueryType.IMDB_ID, idToSearch2).get(0);
+        Movie movie2 = query.searchById(idToSearch2);
         assertEquals(idToSearch2, movie2.getImdbID());
 
         String idToSearch3 = "tt0412142";
-        Movie movie3 = query.search(QueryType.IMDB_ID, idToSearch3).get(0);
+        Movie movie3 = query.searchById(idToSearch3);
         assertEquals(idToSearch3, movie3.getImdbID());
     }
 
     @Test
     void searchByTitleTest() {
         String title = "soul";
-        List<Movie> movies = query.search(QueryType.TITLE, title);
+        List<Movie> movies = query.searchByTitle(title);
 
         assertEquals(10, movies.size());
 
         String idToSearch = "tt2948372";
-        Movie mov = query.search(QueryType.IMDB_ID, idToSearch).get(0);
+        Movie mov = query.searchById(idToSearch);
 
         assertTrue(movies.contains(mov));
 
 
         String title2 = "house";
-        List<Movie> movies2 = query.search(QueryType.TITLE, title2);
+        List<Movie> movies2 = query.searchByTitle(title2);
 
         assertEquals(10, movies2.size());
 
         String idToSearch2 = "tt0412142";
-        Movie mov2 = query.search(QueryType.IMDB_ID, idToSearch2).get(0);
+        Movie mov2 = query.searchById(idToSearch2);
 
         assertTrue(movies2.contains(mov2));
 
 
         String title3 = "Russia";
-        List<Movie> movies3 = query.search(QueryType.TITLE, title3);
+        List<Movie> movies3 = query.searchByTitle(title3);
 
         assertEquals(10, movies3.size());
 
         String idToSearch3 = "tt0220264";
-        Movie mov3 = query.search(QueryType.IMDB_ID, idToSearch3).get(0);
+        Movie mov3 = query.searchById(idToSearch3);
 
         assertEquals(idToSearch3, mov3.getImdbID());
 
     }
-*/
-
 
 
 }
