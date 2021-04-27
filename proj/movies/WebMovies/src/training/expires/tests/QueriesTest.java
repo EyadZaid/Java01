15c635f380061d3785a35528c494fb93afcea42f
EyadZaid <eyad.zaid@hotmail.com>
2021-04-27 13:07:30 +0300

    Web Movies: test Updated

--- proj/movies/WebMovies/src/training/expires/tests/QueriesTest.java
+++ proj/movies/WebMovies/src/training/expires/tests/QueriesTest.java
@@ -3,24 +3,25 @@ package training.expires.tests;
 import org.junit.jupiter.api.Test;
 import training.expires.data.Movie;
 import training.expires.logic.queries.IQuery;
+import training.expires.logic.queries.QueryType;
+import training.expires.logic.queries.UserQuery;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueriesTest {
-    private IQuery query;
+    private UserQuery query;
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        query = UserQuery.getInstance();
     }
 
-    /*
     @Test
     void searchByImdbIdTest() {
-        query = new SearchByImdbId();
         String idToSearch = "tt2948372";
-        Movie movie = query.search(idToSearch).get(0);
+        Movie movie = query.search(QueryType.IMDB_ID, idToSearch).get(0);
         System.out.println(movie);
 
         assertEquals(idToSearch, movie.getImdbID());
@@ -28,18 +29,16 @@ class QueriesTest {
 
     @Test
     void searchByTitleTest() {
-        query = new SearchByTitle();
         String title = "soul";
-        List<Movie> movies = query.search(title);
+        List<Movie> movies = query.search(QueryType.TITLE, title);
 
         assertEquals(10, movies.size());
 
-        query = new SearchByImdbId();
         String idToSearch = "tt2948372";
-        Movie mov = query.search(idToSearch).get(0);
+        Movie mov = query.search(QueryType.IMDB_ID, idToSearch).get(0);
 
         assertTrue(movies.contains(mov));
+
     }
 
-     */
 }
