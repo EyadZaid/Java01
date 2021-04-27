b2ad3b55a8bc9d84a1421f4b3e9830122b1f9a9e
EyadZaid <eyad.zaid@hotmail.com>
2021-04-27 21:06:16 +0300

    Web Movies: Implement user interface

--- proj/movies/WebMovies/src/training/expires/Main.java
+++ proj/movies/WebMovies/src/training/expires/Main.java
@@ -1,17 +1,22 @@
 package training.expires;
 
-import training.expires.logic.queries.Query;
+import training.expires.ui.UserInterface;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        UserInterface ui = new UserInterface();
+        ui.executeApp();
+
+        /*
         Query query = Query.getInstance();
 
         System.out.println(query.searchById("tt0412142"));
         System.out.println(query.searchById("tt0220264"));
         System.out.println(query.searchByTitle("soul"));
         query.shutdown();
+         */
     }
 
 }
--- proj/movies/WebMovies/src/training/expires/ui/UserInterface.java
+++ proj/movies/WebMovies/src/training/expires/ui/UserInterface.java
@@ -1,5 +1,69 @@
 package training.expires.ui;
 
+import training.expires.data.Movie;
+import training.expires.logic.queries.Query;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class UserInterface {
+    private final Query query;
+
+    public UserInterface(){
+        query = Query.getInstance();
+    }
+
+    public void executeApp() {
+        boolean inputLoop = true;
+
+        while (inputLoop){
+            System.out.println("Enter 1 to search by ImdbId");
+            System.out.println("Enter 2 to Search by Title");
+            System.out.println("Enter 3 to exit");
+
+            Scanner reader = new Scanner(System.in);
+            char input = reader.next().charAt(0);
+
+            switch (input){
+                case '1' -> searchByImdbId();
+                case '2' -> searchByTitle();
+                case '3' -> inputLoop = false;
+            }
+        }
+        query.shutdown();
+    }
+
+
+    private void searchByImdbId(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter imdbId:");
+        String inputSearch = scanner.nextLine();
+
+        Movie movie = query.searchById(inputSearch);
+
+        if (movie != null){
+            System.out.println(movie);
+        }
+        else {
+            System.out.println("Movie does not exist");
+        }
+    }
+
+
+    private void searchByTitle(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter title:");
+        String inputSearch = scanner.nextLine();
+
+        List<Movie> moviesList = query.searchByTitle(inputSearch);
+
+        if (moviesList != null && moviesList.size() > 0){
+            System.out.println(moviesList);
+        }
+        else {
+            System.out.println("Movie does not exist");
+        }
+    }
+
 
 }
