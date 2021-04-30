08cb667177158fee79057f18d74081efe2c65bb5
EyadZaid <eyad.zaid@hotmail.com>
2021-04-30 14:33:30 +0300

    ChatClient: main updated

--- ds/ChatClient/src/training/expires/Main.java
+++ ds/ChatClient/src/training/expires/Main.java
@@ -1,9 +1,15 @@
 package training.expires;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-
+        try {
+            new ChatClient();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
