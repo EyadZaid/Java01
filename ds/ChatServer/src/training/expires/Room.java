c149609aba492aac8f962fc4ce12963afe9256f0
EyadZaid <eyad.zaid@hotmail.com>
2021-05-02 13:39:04 +0300

    ChatServer: Implement Room class

--- ds/ChatServer/src/training/expires/Room.java
+++ ds/ChatServer/src/training/expires/Room.java
@@ -1,5 +1,35 @@
 package training.expires;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
+    private final List<User> users;
+    private final int number;
+
+    public Room(int number) {
+        users = new ArrayList<>();
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void sendMessage(String msg) throws IOException {
+        for (var u : users) {
+            u.sendMessage(msg);
+        }
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void removeUser(User user) {
+        users.remove(user);
+    }
 
 }
