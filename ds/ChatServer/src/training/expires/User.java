36791c58615c297186effc4eff80765374f41157
EyadZaid <eyad.zaid@hotmail.com>
2021-05-02 14:28:03 +0300

    ChatServer: Implement User class

--- ds/ChatServer/src/training/expires/User.java
+++ ds/ChatServer/src/training/expires/User.java
@@ -1,4 +1,33 @@
 package training.expires;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class User {
+    private final Socket socket;
+    private final PrintWriter writer;
+    private Room room;
+
+    public User(Socket socket, Room room) throws IOException{
+        this.socket = socket;
+        this.room = room;
+        writer = new PrintWriter(socket.getOutputStream());
+    }
+
+    //--------------------------------------------
+    public void sendMessage(String msg) {
+        if (!socket.isClosed()) {
+            writer.println(msg);
+            writer.flush();
+        }
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
 }
