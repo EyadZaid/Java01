a9006a1ce470a0f0527dff8f6c3ee6dd287cd049
EyadZaid <eyad.zaid@hotmail.com>
2021-05-04 12:43:19 +0300

    ChatServer: added QuitCommand

---
+++ ds/ChatServer/src/training/expires/commands/QuitCommand.java
@@ -0,0 +1,10 @@
+package training.expires.commands;
+
+import training.expires.User;
+
+public class QuitCommand implements ICommand {
+    @Override
+    public void execute(String input, User user) {
+
+    }
+}
