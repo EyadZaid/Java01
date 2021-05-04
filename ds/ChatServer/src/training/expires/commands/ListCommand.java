8e4032b0684cab4f7b8f6fdbc1635803ec2713de
EyadZaid <eyad.zaid@hotmail.com>
2021-05-04 12:29:21 +0300

    ChatServer: added list command

---
+++ ds/ChatServer/src/training/expires/commands/ListCommand.java
@@ -0,0 +1,11 @@
+package training.expires.commands;
+
+import training.expires.User;
+
+public class ListCommand implements ICommand {
+
+    @Override
+    public void execute(String input, User user) {
+
+    }
+}
