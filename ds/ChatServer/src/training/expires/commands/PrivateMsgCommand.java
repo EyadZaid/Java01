a533b23f7c6239a8a69ff45d6dce0c6a2f9fed8a
EyadZaid <eyad.zaid@hotmail.com>
2021-05-04 23:06:41 +0300

    ChatServer: Implement PrivateMsgCommand

--- ds/ChatServer/src/training/expires/commands/PrivateMsgCommand.java
+++ ds/ChatServer/src/training/expires/commands/PrivateMsgCommand.java
@@ -1,11 +1,25 @@
 package training.expires.commands;
 
+import training.expires.ChatManager;
 import training.expires.userHandler.User;
 
 public class PrivateMsgCommand implements ICommand {
 
     @Override
     public void execute(String input, User user) {
+        String[] arrInput = input.split(" ", 2);
 
+        if (arrInput.length < 3) {
+            return;
+        }
+
+        String username = arrInput[1].trim().toLowerCase();
+        User toUser = ChatManager.getInstance().getUserByName(username);
+
+        if (toUser == null) {
+            return;
+        }
+
+        toUser.sendMessage(input, user);
     }
 }
