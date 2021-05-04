ba69609d72cfbaa79aac93164b16ee92324f0974
EyadZaid <eyad.zaid@hotmail.com>
2021-05-04 12:45:05 +0300

    ChatServer: added PrivateMsgCommand

---
+++ ds/ChatServer/src/training/expires/commands/PrivateMsgCommand.java
@@ -0,0 +1,11 @@
+package training.expires.commands;
+
+import training.expires.User;
+
+public class PrivateMsgCommand implements ICommand {
+
+    @Override
+    public void execute(String input, User user) {
+
+    }
+}
