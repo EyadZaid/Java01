0550cd5acdcc674bad1e9a871cb24b90ebd0d582
EyadZaid <eyad.zaid@hotmail.com>
2021-05-04 22:47:22 +0300

    ChatServer: Updated leave room

--- ds/ChatServer/src/training/expires/commands/LeaveCommand.java
+++ ds/ChatServer/src/training/expires/commands/LeaveCommand.java
@@ -7,12 +7,6 @@ public class LeaveCommand implements ICommand {
 
     @Override
     public void execute(String input, User user) {
-        String[] arrInput = input.split(" ");
-
-        if (arrInput.length < 2) {
-            return;
-        }
-
         Room room = user.getRoom();
         if (room != null) {
             room.removeUser(user);
