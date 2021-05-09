2c11b6e034ac2a5ddd6906ffaf9b2758baf54835
EyadZaid <eyad.zaid@hotmail.com>
2021-05-09 12:51:06 +0300

    Exam-02: Add test BigNumberTest

---
+++ ds/Exam-02/src/tests/BigNumberTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import training.expires.BigNumber;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BigNumberTest {
+
+    @Test
+    void bigNumberTest() {
+        BigNumber number1 = new BigNumber("1234321");
+        BigNumber number2 = new BigNumber("100");
+
+        BigNumber result = BigNumber.multiple(number1, number2);
+        String expected = "123432100";
+        assertEquals(expected, result.convertToString());
+
+        result = BigNumber.add(number1, number2);
+        expected = "1234421";
+        assertEquals(expected, result.convertToString());
+
+        result = BigNumber.absoluteNumber(number1);
+        expected = "1234321";
+        assertEquals(expected, result.convertToString());
+
+        boolean isPalindrome = BigNumber.checkPalindrome(number1);
+        assertTrue(isPalindrome);
+    }
+}
