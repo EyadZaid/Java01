329eabeb3005a95f964bbe27d6195314b1a12053
EyadZaid <eyad.zaid@hotmail.com>
2021-05-10 01:57:13 +0300

    Exam-02: Start implement ThreadSafeContainer

---
+++ ds/Exam-02/src/training/expires/ThreadSafeContainer.java
@@ -0,0 +1,41 @@
+package training.expires;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ThreadSafeContainer<T> {
+    private final Map<Integer, Queue<T>> container;
+    private final List<Integer> threadsID;
+
+    public ThreadSafeContainer() {
+        this.container = new ConcurrentHashMap<>();
+        this.threadsID = new ArrayList<>();
+    }
+
+    public void put(T item, int threadID) {
+        var queue = container.get(threadID);
+        if (queue == null) {
+            Queue<T> q = new LinkedList<>();
+            q.add(item);
+            container.put(threadID, q);
+        }
+        else {
+            queue.add(item);
+        }
+    }
+
+    public T[] get() {
+        T[] twoItems = (T[]) new Object[2];
+
+        for (var id : threadsID) {
+            var queue = container.get(id);
+            if (queue.size() > 1) {
+                twoItems[0] = queue.remove();
+                twoItems[1] = queue.remove();
+                return twoItems;
+            }
+        }
+        return twoItems;  ///////////////////// wait()
+    }
+
+}
