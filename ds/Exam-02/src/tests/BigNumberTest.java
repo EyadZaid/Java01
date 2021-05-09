87f69b439b5322b5cf2bcdd93625aabcd3cfe721
EyadZaid <eyad.zaid@hotmail.com>
2021-05-09 17:51:40 +0300

    Exam-02: test BigNumber updated

--- ds/Exam-02/src/tests/BigNumberTest.java
+++ ds/Exam-02/src/tests/BigNumberTest.java
@@ -8,25 +8,42 @@ import static org.junit.jupiter.api.Assertions.*;
 class BigNumberTest {
 
     @Test
-    void bigNumberTest() {
+    void addTest() {
         BigNumber number1 = new BigNumber("1234321");
         BigNumber number2 = new BigNumber("100");
 
         BigNumber result = BigNumber.add(number1, number2);
         String expected = "1234421";
         assertEquals(expected, result.convertToString());
+    }
 
-        result = BigNumber.absoluteNumber(number1);
-        expected = "1234421";
+    @Test
+    void mulTest() {
+        BigNumber number1 = new BigNumber("1234321");
+        BigNumber number2 = new BigNumber("100");
+
+        BigNumber result = BigNumber.multiply(number1, number2);
+        String expected = "123432100";
         assertEquals(expected, result.convertToString());
+    }
 
+    @Test
+    void isPalindromeTest() {
+        BigNumber number1 = new BigNumber("1234321");
 
-        expected = "1234321";
-        assertEquals(expected, number1.convertToString());
-
-        /*
         boolean isPalindrome = BigNumber.checkPalindrome(number1);
         assertTrue(isPalindrome);
-*/
     }
+
+
+    @Test
+    void absoluteNumberTest() {
+        BigNumber number1 = new BigNumber("1234321");
+
+        BigNumber result = BigNumber.absoluteNumber(number1);
+        String expected = "1234321";
+        assertEquals(expected, result.convertToString());
+    }
+
+
 }
--- ds/Exam-02/src/training/expires/BigNumber.java
+++ ds/Exam-02/src/training/expires/BigNumber.java
@@ -1,10 +1,5 @@
 package training.expires;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class BigNumber {
 
     static class Node {
