118cf33c6647c3f322d1f3fc0f50ef05b5ab3be8
EyadZaid <eyad.zaid@hotmail.com>
2021-05-16 07:29:19 +0300

    SqliteJdbc: implement Track class

---
+++ ds/SqliteJdbc/src/main/java/com/training/experis/Track.java
@@ -0,0 +1,30 @@
+package com.training.experis;
+
+public class Track {
+    private final String trackId;
+    private final String name;
+    private final String albumId;
+    private final String mediaTypeId;
+    private final String genreId;
+    private final String composer;
+    private final int milliseconds;
+    private final int bytes;
+    private final float unitPrice;
+
+    public Track(String trackId, String name, String albumId, String mediaTypeId,
+                 String genreId, String composer, int milliseconds, int bytes, float unitPrice) {
+        this.trackId = trackId;
+        this.name = name;
+        this.albumId = albumId;
+        this.mediaTypeId = mediaTypeId;
+        this.genreId = genreId;
+        this.composer = composer;
+        this.milliseconds = milliseconds;
+        this.bytes = bytes;
+        this.unitPrice = unitPrice;
+    }
+
+
+
+
+}
