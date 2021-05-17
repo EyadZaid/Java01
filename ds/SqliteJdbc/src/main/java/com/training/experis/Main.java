327007e17b1f1c0e94298c3956464b6c3f3ba5ff
EyadZaid <eyad.zaid@hotmail.com>
2021-05-17 15:36:20 +0300

    SqliteJdbc: Implement create invoice

--- ds/SqliteJdbc/src/main/java/com/training/experis/Main.java
+++ ds/SqliteJdbc/src/main/java/com/training/experis/Main.java
@@ -1,6 +1,7 @@
 package com.training.experis;
 
 import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -26,6 +27,13 @@ public class Main {
         System.out.println("\nEnter album id: ");
         int albumId = scanner.nextInt();
         System.out.println(getAllTracksByAlbumId(albumId));
+
+        System.out.println("\nEnter track id: ");
+        int trackId = scanner.nextInt();
+
+        createInvoice(user, trackId, 1);
+
+
     }
 
     public static Connection connect() throws SQLException {
@@ -37,7 +45,8 @@ public class Main {
 
     public static User getUserById(int id) {
         User user = null;
-        String sql = "SELECT FirstName, City, Email FROM customers" +
+        String sql = "SELECT FirstName, Email, City, Address, State, Country, " +
+                "PostalCode FROM customers" +
                 " where CustomerId = " + id;
 
         try (var conn = connect();
@@ -48,7 +57,11 @@ public class Main {
                 String name = rs.getString("FirstName");
                 String city = rs.getString("City");
                 String email = rs.getString("Email");
-                user = new User(id, name, email, city);
+                String address = rs.getString("Address");
+                String state = rs.getString("State");
+                String country = rs.getString("Country");
+                String postalCode = rs.getString("PostalCode");
+                user = new User(id, name, email, city, address, state, country, postalCode);
             }
 
         } catch (SQLException e) {
@@ -109,10 +122,18 @@ public class Main {
         return tracks;
     }
 
-    public static void createInvoice() {
-        String sql = "INSERT INTO invoices (CustomerId, InvoiceDate, BillingAddress, BillingCity, " +
-                "BillingState, BillingCountry, BillingPostalCode, Total) " +
-                "VALUES (5, '', 'aaa', 'bbb', 'ccc', 'ddd', 'eee', 0.99);";
+    public static void createInvoice(User user, int trackId, int quantity) {
+        var sqlPattern = """
+                INSERT INTO invoices (CustomerId, InvoiceDate, BillingAddress, BillingCity, 
+                BillingState, BillingCountry, BillingPostalCode, Total) 
+                VALUES (%d, '%s', '%s', '%s', '%s', '%s', '%s', %f);
+                """;
+
+        var date = java.time.LocalDate.now();
+        float total = 20.5f;
+
+        var sql = String.format(sqlPattern, user.getId(), date, user.getAddress(), user.getCity(),
+                user.getState(), user.getCountry(), user.getPostalCode(), total);
 
         Connection conn = null;
         Statement stmt = null;
--- ds/SqliteJdbc/src/main/java/com/training/experis/User.java
+++ ds/SqliteJdbc/src/main/java/com/training/experis/User.java
@@ -5,12 +5,53 @@ public class User {
     private final String name;
     private final String email;
     private final String city;
+    private final String address;
+    private final String state;
+    private final String country;
+    private final String postalCode;
 
-    public User(int id, String name, String email, String city) {
+    public User(int id, String name, String email, String city, String address, String state,
+                String country, String postalCode) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.city = city;
+        this.address = address;
+        this.state = state;
+        this.country = country;
+        this.postalCode = postalCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
     }
 
     @Override
