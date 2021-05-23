ade146a36175089b390ba5a4af092fc6188f9542
EyadZaid <eyad.zaid@hotmail.com>
2021-05-23 11:45:15 +0300

    interview_prep: Implement MissingNumber class

---
+++ ds/interview_prep/src/com/experis/MissingNumber.java
@@ -0,0 +1,16 @@
+package com.experis;
+
+public class MissingNumber {
+
+    static int missingNum(int arr[]) {
+        int n = arr.length;
+        int itemsSum = n * (n + 1) / 2;
+        int sum = 0;
+
+        for(int i = 0; i < n - 1; i++) {
+            sum += arr[i];
+        }
+
+        return itemsSum - sum;
+    }
+}
