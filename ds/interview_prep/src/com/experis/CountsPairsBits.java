9410077cbd218020dfadee7dfe18a167c7b92539
EyadZaid <eyad.zaid@hotmail.com>
2021-05-26 23:51:11 +0300

    interview_prep: CountsPairsBits updated

--- ds/interview_prep/src/com/experis/CountsPairsBits.java
+++ ds/interview_prep/src/com/experis/CountsPairsBits.java
@@ -1,6 +1,11 @@
 package com.experis;
 
-public class CountsPairs {
+public class CountsPairsBits {
+
+    /**
+     * given an unsigned long (64 bits) write a function that counts the number of pairs of 1's :
+     * i.e. : 00011001110101 has 3 pairs of 1
+     */
 
     public static int countsPairsBits(long n) {
         int result = 0;
@@ -23,6 +28,24 @@ public class CountsPairs {
     }
 
 
+    public static int countsPairsBits2(long n) {
+        int count = 0;
+
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n >>= 1;
+        }
+
+        return count/2;
+    }
+
+
+    /**
+     * Same as previous but we count pairs of 1s (2 consecutive bits that are 1 make one pair).
+     * i.e. : 00101101110  has 3 pairs
+     */
 
     public static int countsPairsBitsConsecutive (long n) {
         int result = 0;
--- ds/interview_prep/src/com/experis/tests/CountsPairsBitsTest.java
+++ ds/interview_prep/src/com/experis/tests/CountsPairsBitsTest.java
@@ -1,18 +1,27 @@
 package com.experis.tests;
 
-import com.experis.CountsPairs;
+import com.experis.CountsPairsBits;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CountsPairsTest {
+class CountsPairsBitsTest {
 
     @Test
     void countsPairsBits() {
         String binaryString="00011001110101";
         long decimal = Long.parseLong(binaryString, 2);
 
-        int expected = CountsPairs.countsPairsBits(decimal);
+        int expected = CountsPairsBits.countsPairsBits(decimal);
+        assertEquals(3, expected);
+    }
+
+    @Test
+    void countsPairsBitsPart2() {
+        String binaryString="00011001110101";
+        long decimal = Long.parseLong(binaryString, 2);
+
+        int expected = CountsPairsBits.countsPairsBits2(decimal);
         assertEquals(3, expected);
     }
 
@@ -21,7 +30,7 @@ class CountsPairsTest {
         String binaryString="00011001110101";
         long decimal = Long.parseLong(binaryString, 2);
 
-        int expected = CountsPairs.countsPairsBitsConsecutive(decimal);
+        int expected = CountsPairsBits.countsPairsBitsConsecutive(decimal);
         assertEquals(2, expected);
     }
 }
