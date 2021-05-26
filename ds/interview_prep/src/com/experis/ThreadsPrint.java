ebb1681586e969209c508b17c501bcfbb490581d
EyadZaid <eyad.zaid@hotmail.com>
2021-05-26 07:08:07 +0300

    interview_prep: Implement ThreadsPrint class

---
+++ ds/interview_prep/src/com/experis/ThreadsPrint.java
@@ -0,0 +1,67 @@
+package com.experis;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ThreadsPrint {
+    private int number = 1;
+    private final int size;
+
+    private final Lock lock;
+    private final Condition oddNumber;
+    private final Condition evenNumber;
+
+    public ThreadsPrint(int size) {
+        this.size =size;
+        lock = new ReentrantLock();
+        oddNumber = lock.newCondition();
+        evenNumber = lock.newCondition();
+    }
+
+    public void printEvenNumber() {
+        lock.lock();
+        try {
+            while (number < size) {
+                while (number % 2 == 1) {
+                    evenNumber.await();
+                }
+
+                System.out.print(number + " ");
+                number++;
+
+                oddNumber.signal();
+            }
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+
+    public void printOddNumber() {
+        lock.lock();
+        try {
+            while (number < size) {
+                while (number % 2 == 0) {
+                    oddNumber.await();
+                }
+
+                System.out.print(number + " ");
+                number++;
+
+                evenNumber.signal();
+            }
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            lock.unlock();
+        }
+    }
+
+}
