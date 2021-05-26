4bde6c77b215cc1b143495a73a9b48a81c0ece5d
EyadZaid <eyad.zaid@hotmail.com>
2021-05-26 08:43:27 +0300

    interview_prep: Added PairNumbers class

---
+++ ds/interview_prep/src/com/experis/PairNumbers.java
@@ -0,0 +1,19 @@
+package com.experis;
+
+public class PairNumbers {
+    private final int x;
+    private final int y;
+
+    public PairNumbers(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
