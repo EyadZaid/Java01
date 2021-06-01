4740e86bea52043d16a3dfd5e831c036f0ece5a3
EyadZaid <eyad.zaid@hotmail.com>
2021-06-01 21:58:18 +0300

    BasicAlgorithms: added test to PairSum class

--- ds/BasicAlgorithms/src/com/experis/PairSum.java
+++ ds/BasicAlgorithms/src/com/experis/PairSum.java
@@ -1,11 +1,14 @@
 package com.experis;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class PairSum {
     public static void pairEqualSum(int[] arr, int sum) {
-        HashSet<Integer> set = new HashSet<>();
+        assert arr != null;
         int num;
+        HashSet<Integer> set = new HashSet<>();
+
         for (int i = 0; i < arr.length; ++i) {
             num = sum - arr[i];
 
@@ -13,6 +16,30 @@ public class PairSum {
                 System.out.println("(" + arr[i] + ", " + num + ")");
             }
             set.add(arr[i]);
+        }
+    }
+
+
+    public static void pairEqualSumWithSort(int[] arr, int sum) {
+        int i, j;
+
+        Arrays.sort(arr);
+        i = 0;
+        j = arr.length - 1;
+
+        while (i < j) {
+            if (arr[i] + arr[j] == sum) {
+                System.out.println("(" + arr[j] + ", " + arr[i] + ")");
+                i++;
+            }
+            else {
+                if (arr[i] + arr[j] < sum) {
+                    i++;
+                }
+                else {
+                    j--;
+                }
+            }
         }
     }
 }
--- ds/BasicAlgorithms/src/com/experis/tests/PairSumTest.java
+++ ds/BasicAlgorithms/src/com/experis/tests/PairSumTest.java
@@ -1,14 +1,23 @@
 package com.experis.tests;
 
 import com.experis.PairSum;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PairSumTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void pairEqualSum() {
         int[] arr = {0,10,-5,30,25,50,60,71,-11,55,91};
+        System.out.println();
         PairSum.pairEqualSum(arr, 55);
+    }
+
+    @Test
+    void pairEqualSumWithSort() {
+        int[] arr = {0,10,-5,30,25,50,60,71,-11,55,91};
+        System.out.println();
+        PairSum.pairEqualSumWithSort(arr, 55);
     }
 }
