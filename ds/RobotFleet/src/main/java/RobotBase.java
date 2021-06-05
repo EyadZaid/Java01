5f33550600eee6f964d82b27eb8687182bdb14d3
EyadZaid <eyad.zaid@hotmail.com>
2021-06-05 13:03:08 +0300

    RobotFleet : RobotBase class added

---
+++ ds/RobotFleet/src/main/java/RobotBase.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RobotBase implements IRobot{
+    private String name;
+    private String callSign;
+    private final List<ITool> tools;
+
+    public RobotBase() {
+        tools = new ArrayList<>();
+    }
+
+    @Override
+    public void dispatch() {
+
+    }
+
+    @Override
+    public void reboot() {
+
+    }
+
+    @Override
+    public void diagnostics() {
+
+    }
+}
