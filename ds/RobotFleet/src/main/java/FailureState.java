8ff7967c5020e5a157f137b91fe95e5c03387c74
EyadZaid <eyad.zaid@hotmail.com>
2021-06-05 19:49:40 +0300

    RobotFleet : ActiveState class added

--- ds/RobotFleet/src/main/java/FailureState.java
+++ ds/RobotFleet/src/main/java/FailureState.java
@@ -18,6 +18,6 @@ public class FailureState implements IRobotState {
 
     @Override
     public IRobotState failure(IRobot robot) {
-        return new FailureState();
+        return this;
     }
 }
