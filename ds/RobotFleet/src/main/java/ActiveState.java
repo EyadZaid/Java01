57bc883ea880c839b10b6376d3520d33290e2052
EyadZaid <eyad.zaid@hotmail.com>
2021-06-05 19:49:47 +0300

    RobotFleet : ActiveState class added

---
+++ ds/RobotFleet/src/main/java/ActiveState.java
@@ -0,0 +1,23 @@
+public class ActiveState implements IRobotState{
+
+    @Override
+    public IRobotState active(IRobot robot) {
+        return this;
+    }
+
+    @Override
+    public IRobotState reboot(IRobot robot) {
+        robot.reboot();
+        return new RebootState();
+    }
+
+    @Override
+    public IRobotState work(IRobot robot) {
+        return new WorkState();
+    }
+
+    @Override
+    public IRobotState failure(IRobot robot) {
+        throw new IllegalStateException();
+    }
+}
