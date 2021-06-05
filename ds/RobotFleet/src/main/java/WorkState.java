61586ac04edff6b694cc5d71414b540b8717aa8e
EyadZaid <eyad.zaid@hotmail.com>
2021-06-05 19:58:44 +0300

    RobotFleet : WorkState class added

---
+++ ds/RobotFleet/src/main/java/WorkState.java
@@ -0,0 +1,21 @@
+public class WorkState implements IRobotState {
+    @Override
+    public IRobotState active(IRobot robot) {
+        return new ActiveState();
+    }
+
+    @Override
+    public IRobotState reboot(IRobot robot) {
+        return new RebootState();
+    }
+
+    @Override
+    public IRobotState work(IRobot robot) {
+        return this;
+    }
+
+    @Override
+    public IRobotState failure(IRobot robot) {
+        return new FailureState();
+    }
+}
