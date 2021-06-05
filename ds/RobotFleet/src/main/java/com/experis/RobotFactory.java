6e71e160b0daa4d7611f2270b3a8efa4f57a9893
EyadZaid <eyad.zaid@hotmail.com>
2021-06-05 23:56:17 +0300

    RobotFleet : implement RobotFactory class

--- ds/RobotFleet/src/main/java/com/experis/RobotFactory.java
+++ ds/RobotFleet/src/main/java/com/experis/RobotFactory.java
@@ -1,7 +1,40 @@
 package com.experis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RobotFactory {
-    public IRobot getRobot(RobotModel model) {
-        return null;
+    public IRobot getRobot(String name, String callSign, RobotModel model) {
+        ToolFactory toolFactory = new ToolFactory();
+        List<ITool> tools = new ArrayList<>();
+
+        switch (model) {
+            case HAL9000 -> {
+                tools.add(toolFactory.getTool(ToolName.LASER_CUTTER));
+                tools.add(toolFactory.getTool(ToolName.REPLICATOR));
+                tools.add(toolFactory.getTool(ToolName.DISRUPTOR));
+                return new RobotBase(name, callSign, model, tools);
+            }
+
+            case TACHIKOMAS -> {
+                tools.add(toolFactory.getTool(ToolName.LASER_CUTTER));
+                tools.add(toolFactory.getTool(ToolName.DISRUPTOR));
+                return new RobotBase(name, callSign, model, tools);
+            }
+
+            case JOHNNY5 -> {
+                tools.add(toolFactory.getTool(ToolName.LASER_CUTTER));
+                tools.add(toolFactory.getTool(ToolName.STATIC_BRUSH));
+                return new RobotBase(name, callSign, model, tools);
+            }
+
+            case MASCHINENMENSCH -> {
+                tools.add(toolFactory.getTool(ToolName.REPLICATOR));
+                tools.add(toolFactory.getTool(ToolName.DISRUPTOR));
+                return new RobotBase(name, callSign, model, tools);
+            }
+
+            default -> { return null; }
+        }
     }
 }
