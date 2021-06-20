7af35f9261446d230127d8c56b2180e92cb11c9a
EyadZaid <eyad.zaid@hotmail.com>
2021-06-20 11:52:59 +0300

    MongoRestApi : initialize new project

---
+++ ds/MongoRestApi/src/main/java/com/experis/mongorestapi/MongoRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.experis.mongorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoRestApiApplication.class, args);
+    }
+
+}
---
+++ ds/MongoRestApi/src/test/java/com/experis/mongorestapi/MongoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experis.mongorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
