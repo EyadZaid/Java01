c08f6c4b1aeecbd78ffaa111595b34de50f04f68
EyadZaid <eyad.zaid@hotmail.com>
2021-06-20 13:56:41 +0300

    MongoRestApi : Language class added

---
+++ ds/MongoRestApi/src/main/java/com/experis/mongorestapi/entities/Language.java
@@ -0,0 +1,28 @@
+package com.experis.mongorestapi.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
+@ToString
+public class Language {
+    @Id
+    private String id;
+    private String name;
+    private String creator;
+    private int year;
+    private float lastVersion;
+
+    public Language() {
+    }
+
+    public Language(String name, String creator, int year, float lastVersion) {
+        this.name = name;
+        this.creator = creator;
+        this.year = year;
+        this.lastVersion = lastVersion;
+    }
+}
