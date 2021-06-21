676c0a035f4fb6d3ebf1f5c44547d104b3cc5325
EyadZaid <eyad.zaid@hotmail.com>
2021-06-21 13:42:58 +0300

    CdrInsight : implement Cdr and Subscriber

--- proj/CdrInsight/src/main/java/com/experis/cdrinsight/entities/Cdr.java
+++ proj/CdrInsight/src/main/java/com/experis/cdrinsight/entities/Cdr.java
@@ -1,4 +1,27 @@
 package com.experis.cdrinsight.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "cdrs")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cdr {
+    @Id
+    private String id;
+    private Subscriber subscriber;
+    private UsageType usageType;
+    private String date;
+    private String time;
+    private int duration;
+    private int bytesReceived;
+    private int bytesTransmitted;
+    private String partyImsi;
+    private String partyMsisdn;
 }
--- proj/CdrInsight/src/main/java/com/experis/cdrinsight/entities/Subscriber.java
+++ proj/CdrInsight/src/main/java/com/experis/cdrinsight/entities/Subscriber.java
@@ -1,4 +1,21 @@
 package com.experis.cdrinsight.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "subscribers")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Subscriber {
+    @Id
+    private String msin;
+    private String imsi;
+    private String imei;
+    private String msisdn;
 }
