505ff14030ae1500e25d05f09b7e153de7f535a4
EyadZaid <eyad.zaid@hotmail.com>
2021-06-22 11:56:45 +0300

    CdrInsight : added DataCdr

---
+++ proj/CdrInsight/src/main/java/com/experis/cdrinsight/entities/DataCdr.java
@@ -0,0 +1,21 @@
+package com.experis.cdrinsight.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "data")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DataCdr {
+    @Id
+    private String id;
+    private LogItem logItem;
+    private int bytesReceived;
+    private int bytesTransmitted;
+}
