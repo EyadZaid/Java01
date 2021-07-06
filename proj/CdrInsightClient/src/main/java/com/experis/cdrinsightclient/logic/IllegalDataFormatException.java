10d87c9937e9b7a08010edf94e9503e3195fd183
EyadZaid <eyad.zaid@hotmail.com>
2021-07-06 21:21:29 +0300

    CdrInsightClient: IllegalDataFormatException added

---
+++ proj/CdrInsightClient/src/main/java/com/experis/cdrinsightclient/logic/IllegalDataFormatException.java
@@ -0,0 +1,9 @@
+package com.experis.cdrinsightclient.logic;
+
+public class IllegalDataFormatException extends Exception{
+
+    public IllegalDataFormatException(String msg) {
+        super(msg);
+    }
+}
+
