495990c007e34328355bed3426a3b133af6bb80f
EyadZaid <eyad.zaid@hotmail.com>
2021-07-06 13:41:37 +0300

    CdrInsightClient: Initialize new project

---
+++ proj/CdrInsightClient/src/main/java/com/experis/cdrinsightclient/CdrInsightClientApplication.java
@@ -0,0 +1,13 @@
+package com.experis.cdrinsightclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdrInsightClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdrInsightClientApplication.class, args);
+    }
+
+}
---
+++ proj/CdrInsightClient/src/test/java/com/experis/cdrinsightclient/CdrInsightClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experis.cdrinsightclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdrInsightClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
