d0bceb9f587be2371b31fdc923ddf465c00aa132
EyadZaid <eyad.zaid@hotmail.com>
2021-07-06 21:21:51 +0300

    CdrInsightClient: FileRead class added

---
+++ proj/CdrInsightClient/src/main/java/com/experis/cdrinsightclient/logic/FileRead.java
@@ -0,0 +1,47 @@
+package com.experis.cdrinsightclient.logic;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileRead implements IInputData {
+    private String line;
+    private final FileReader reader;
+    private final BufferedReader bufferedReader;
+
+    public FileRead(String fileName) throws FileNotFoundException {
+        line = "";
+        reader = new FileReader(fileName);
+        bufferedReader = new BufferedReader(reader);
+    }
+
+    @Override
+    public String readline() {
+        try {
+            if ((line = bufferedReader.readLine()) != null) {
+                return line;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    @Override
+    public Boolean isEnd() {
+        if (line == null || line.equals("")){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void close() {
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
