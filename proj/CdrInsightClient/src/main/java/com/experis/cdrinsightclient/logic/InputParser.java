36a0f36c1a4fc24713dd17c3e2f411f6b3f1e2be
EyadZaid <eyad.zaid@hotmail.com>
2021-07-07 17:00:47 +0300

    CdrInsightClient: Implement read cdr file

--- proj/CdrInsightClient/src/main/java/com/experis/cdrinsightclient/logic/InputParser.java
+++ proj/CdrInsightClient/src/main/java/com/experis/cdrinsightclient/logic/InputParser.java
@@ -6,15 +6,13 @@ import com.experis.cdrinsightclient.model.UsageType;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
 
 public class InputParser implements IDataFormat {
 
     @Override
     public Cdr inputParse(String line) throws IllegalDataFormatException {
 
-        String[] details = line.split("\\|");
+        String[] details = line.split("\\|", -1);
         if (details.length != 12){
             throw new IllegalDataFormatException("Invalid file format");
         }
@@ -22,17 +20,27 @@ public class InputParser implements IDataFormat {
         String SequenceNum = details[0];
         String imsi = details[1];
         String imei = details[2];
-        UsageType usageType = UsageType.valueOf(details[3]);
+
+        UsageType usageType;
+        switch (details[3]) {
+            case "SMS-MT": usageType = UsageType.SMS_MT;
+            break;
+
+            case "SMS-O": usageType = UsageType.SMS_MO;
+            break;
+
+            default: usageType = UsageType.valueOf(details[3]);
+        }
+
         String msisdn = details[4];
 
         // --------------  To do: Function
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-M-yyyy hh:mm:ss", Locale.ENGLISH);
-        formatter.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-        String dateInString = details[5] + details[6];
+        String dateInString = details[5] + " " + details[6];
         Date date = null;
         try {
-            date = formatter.parse(dateInString);
+            date = simpleDateFormat.parse(dateInString);
         } catch (ParseException e) {
             e.printStackTrace();
         }
